5714bdd77c24ef225e03c6596a7405d2597b9a53
Algen Dela Cruz <algen.delacruz21@gmail.com>
2025-02-21 09:46:22 +0800

    Refactor passlevel command, added removelevel

---
+++ levels.js
@@ -0,0 +1,12 @@
+// levels.js
+module.exports = [
+  "1 Diaphragmatic Support",
+  "2 Open Throat",
+  "3 Vocal Tract Shaping",
+  "4 Build Head Voice",
+  "5 Passaggio & Mixed Voice",
+  "6 Glottal Compression",
+  "7 Contiguous Phrase Singing",
+  "8 Hyper Glottal Compression",
+  "9 Master Sensei"
+];
