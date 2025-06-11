2cbdcc758f79bbf4a2750c59ba00445b401fe6fa
Algen <algen.delacruz21@gmail.com>
2025-06-11 11:14:35 +0800

    Fixing skill bug

--- commands/passlevel.js
+++ commands/passlevel.js
@@ -67,6 +67,8 @@ module.exports = {
                 '1 Diaphragmatic Support': '<:belt1:1382187715986198618>'
             };
 
+            console.log('role', role);
+
             let icon = beltEmojis[role];
 
             console.log("icon", icon);
